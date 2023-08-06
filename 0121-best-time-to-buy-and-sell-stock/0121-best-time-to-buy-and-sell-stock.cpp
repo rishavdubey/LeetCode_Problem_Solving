f7579e222b30ea07182dfbbb17c10fda677d664b
Rishav Kumar <55137788+rishavdubey@users.noreply.github.com>
2023-08-06 20:41:12 +0530

    Time: 91 ms (95.84%), Space: 93.3 MB (29.56%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-//         int n=prices.size();
-//         int left[n];
-//         int right[n];
-//         left[0]=prices[0];
-//         for(int i=1;i<n;i++){
-//             left[i]=min(left[i-1],prices[i]);
-//         }
-//         right[n-1]=prices[n-1];
-//         for(int i=n-2;i>=0;i--){
-//             right[i]=max(right[i+1],prices[i]);
-            
-//         }
-//         int ans=0;
-//         for(int i=0;i<n;i++){
-//             ans=max(ans,right[i]-left[i]);
-//         }
-//         return ans;
         
-        int mini=INT_MAX;
-        int ans=0;
-        for(int i=0;i<prices.size();i++){
-            mini=min(mini,prices[i]);
-            ans=max(ans,prices[i]-mini);
+        int profit=0;
+        int buyPrice=prices[0];
+        for(int i=1;i<prices.size();i++){
+            profit=max(profit,prices[i]-buyPrice);
+            if(buyPrice>prices[i]){
+                buyPrice=prices[i];
+            }
+            
         }
-        return ans;
+        return profit;
         
     }
 };
