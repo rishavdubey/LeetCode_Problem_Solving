7d516f200f251b4adb68e205863e63095a7b4448
Rishav Kumar <55137788+rishavdubey@users.noreply.github.com>
2024-09-06 13:24:12 +0530

    Time: 34 ms (97.32%), Space: 48.6 MB (26.61%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -2,63 +2,58 @@ struct Node{
     Node* links[26];
     bool flag=false;
     bool containsKey(char ch){
-        return (links[ch-'a'] != NULL);
+        return links[ch-'a']!=NULL;
     }
-    
     void put(char ch,Node* node){
         links[ch-'a']=node;
     }
-    
     Node* get(char ch){
         return links[ch-'a'];
     }
     void setEnd(){
         flag=true;
     }
-    bool  isEnd(){
+    bool getEnd(){
         return flag;
     }
 };
 
 class Trie {
-private: 
-    Node* root;
-
 public:
+    Node* root;
     Trie() {
         root=new Node();
-        
     }
     
     void insert(string word) {
         Node* node=root;
-        for(int i=0;i<word.length();i++){
-            if(! node->containsKey(word[i])){
+        for(int i=0;i<word.size();i++){
+            if(!node->containsKey(word[i])){
                 node->put(word[i],new Node());
             }
             node=node->get(word[i]);
         }
         node->setEnd();
-        
     }
     
     bool search(string word) {
         Node* node=root;
-        for(int i=0;i<word.length();i++){
+        for(int i=0;i<word.size();i++){
             if(!node->containsKey(word[i])){
                 return false;
             }
             node=node->get(word[i]);
         }
-        return node->isEnd();
+        return node->getEnd();
         
     }
     
     bool startsWith(string prefix) {
         Node* node=root;
-        for(int i=0;i<prefix.length();i++){
-            if(!node->containsKey(prefix[i]))
+        for(int i=0;i<prefix.size();i++){
+            if(!node->containsKey(prefix[i])){
                 return false;
+            }
             node=node->get(prefix[i]);
         }
         return true;
